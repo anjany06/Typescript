7007031e08fb2234cf5aa346c1bc71bfb625b714
Anjany Pandey <anjany.pandey06@gmail.com>
2025-02-11 17:58:21 +0530

    Core Data types L1

---
+++ L1/app.js
@@ -0,0 +1,7 @@
+function add(num1, num2, str) {
+    console.log(num1, num2, str);
+}
+var n1 = 50;
+var n2 = true;
+var s = "hello";
+add(n1, n2, s);
---
+++ L1/app.ts
@@ -0,0 +1,7 @@
+function add(num1 : number, num2: boolean, str: string){
+  console.log(num1, num2, str);
+}
+const n1 = 50;
+const n2 = true;
+const s = "hello";
+add(n1, n2, s);
