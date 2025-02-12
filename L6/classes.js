db4274f28f733b8d806b4c3779336a5a424b6291
Anjany Pandey <anjany.pandey06@gmail.com>
2025-02-12 23:21:29 +0530

    learned about read-only protected inheritance and overrding in ts

--- L6/classes.js
+++ L6/classes.js
@@ -18,9 +18,66 @@
 // }
 // accountingCopy.describe(); // result -> [department undefined] as this object does not have any name property
 //Access Modifiers -> public, private
+// class Department {
+//   name: string; // we can initialize this name:string ="subham"
+//   private employee: string[];
+//   constructor(n: string){
+//     this.name = n;
+//     this.employee = [];
+//   }
+//   // yha pe this:Department ensures ki isme dep class ka hi ek obj hona chaiye
+//   describe( this:Department){
+//     console.log("Department", this.name);
+//   }
+//   addEmployee(emp: string){
+//     this.employee.push(emp);
+//   }
+//   printEmployee( this:Department){
+//     console.log("Number of emplpyee : ", this.employee.length);
+//     console.log(this.employee);
+//   }
+// }
+// const accounting = new Department("Accounting");
+// accounting.describe();
+// accounting.addEmployee("subham");
+// accounting.addEmployee("rohan");
+// // accounting.employee = ["mac"];// yeh directly accessible hai so isko aesa ni chahte hm and isko ab hm pvt kr denge
+// accounting.printEmployee();
+//readonly Access Modifiers
+// readonly lagane se sirf woh ek bar initialize ho skti hai and hm usko baad me change bhi ni kr skte
+// class Department {
+//   name: string; // we can initialize this name:string ="subham"
+//   private employee: string[];
+//   private readonly id: string;
+//   constructor(n: string){
+//     this.name = n;
+//     this.id ="d1"; // only once you can initialize
+//     this.employee = [];
+//   }
+//   // yha pe this:Department ensures ki isme dep class ka hi ek obj hona chaiye
+//   describe( this:Department){
+//     console.log("Department", this.name);
+//   }
+//   addEmployee(emp: string){
+//     this.employee.push(emp);
+//   }
+//   printEmployee( this:Department){
+//     console.log("Number of emplpyee : ", this.employee.length);
+//     console.log(this.employee);
+//   }
+// }
+// const accounting = new Department("Accounting");
+// accounting.describe();
+// accounting.addEmployee("subham");
+// accounting.addEmployee("rohan");
+// // accounting.employee = ["mac"];// yeh directly accessible hai so isko aesa ni chahte hm and isko ab hm pvt kr denge
+// accounting.printEmployee();
+//! Inheritence
+//->overidde and protected
 class Department {
-    constructor(n) {
+    constructor(id, n) {
         this.name = n;
+        this.id = id;
         this.employee = [];
     }
     // yha pe this:Department ensures ki isme dep class ka hi ek obj hona chaiye
@@ -35,10 +92,27 @@ class Department {
         console.log(this.employee);
     }
 }
-const accounting = new Department("Accounting");
-accounting.describe();
-accounting.addEmployee("subham");
-accounting.addEmployee("rohan");
-// accounting.employee = ["mac"];// yeh directly accessible hai so isko aesa ni chahte hm and isko ab hm pvt kr denge
-accounting.printEmployee();
-//readnly
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super(id, "Accounting"); // child class k constructor se phle parent class k constructor ko call lgane k super ka use
+        this.reports = reports;
+    }
+    addReports(text) {
+        this.reports.push(text);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+    addEmployee(emp) {
+        if (emp == "Patel") {
+            return;
+        }
+        this.employee.push(emp);
+    }
+}
+const accDep = new AccountingDepartment("c1", ["bugs"]);
+accDep.describe();
+accDep.addReports("complier issue");
+accDep.printReports();
+accDep.addEmployee("varun");
+accDep.printEmployee();
--- L6/classes.ts
+++ L6/classes.ts
@@ -27,11 +27,89 @@
 
 //Access Modifiers -> public, private
 
+// class Department {
+//   name: string; // we can initialize this name:string ="subham"
+//   private employee: string[];
+//   constructor(n: string){
+//     this.name = n;
+//     this.employee = [];
+//   }
+//   // yha pe this:Department ensures ki isme dep class ka hi ek obj hona chaiye
+//   describe( this:Department){
+//     console.log("Department", this.name);
+//   }
+
+//   addEmployee(emp: string){
+//     this.employee.push(emp);
+//   }
+//   printEmployee( this:Department){
+//     console.log("Number of emplpyee : ", this.employee.length);
+//     console.log(this.employee);
+//   }
+// }
+
+// const accounting = new Department("Accounting");
+// accounting.describe();
+// accounting.addEmployee("subham");
+// accounting.addEmployee("rohan");
+
+
+// // accounting.employee = ["mac"];// yeh directly accessible hai so isko aesa ni chahte hm and isko ab hm pvt kr denge
+
+
+// accounting.printEmployee();
+
+
+//readonly Access Modifiers
+
+// readonly lagane se sirf woh ek bar initialize ho skti hai and hm usko baad me change bhi ni kr skte
+// class Department {
+//   name: string; // we can initialize this name:string ="subham"
+//   private employee: string[];
+//   private readonly id: string;
+//   constructor(n: string){
+//     this.name = n;
+//     this.id ="d1"; // only once you can initialize
+//     this.employee = [];
+//   }
+//   // yha pe this:Department ensures ki isme dep class ka hi ek obj hona chaiye
+//   describe( this:Department){
+//     console.log("Department", this.name);
+//   }
+
+//   addEmployee(emp: string){
+//     this.employee.push(emp);
+//   }
+//   printEmployee( this:Department){
+//     console.log("Number of emplpyee : ", this.employee.length);
+//     console.log(this.employee);
+//   }
+// }
+
+// const accounting = new Department("Accounting");
+// accounting.describe();
+// accounting.addEmployee("subham");
+// accounting.addEmployee("rohan");
+
+
+// // accounting.employee = ["mac"];// yeh directly accessible hai so isko aesa ni chahte hm and isko ab hm pvt kr denge
+
+
+// accounting.printEmployee();
+
+
+
+//! Inheritence
+
+//->overidde and protected
+
 class Department {
   name: string; // we can initialize this name:string ="subham"
-  private employee: string[];
-  constructor(n: string){
+  protected employee: string[];
+  private readonly id: string;
+  constructor(id: string, n: string){
     this.name = n;
+    this.id =id;
     this.employee = [];
   }
   // yha pe this:Department ensures ki isme dep class ka hi ek obj hona chaiye
@@ -48,16 +126,29 @@ class Department {
   }
 }
 
-const accounting = new Department("Accounting");
-accounting.describe();
-accounting.addEmployee("subham");
-accounting.addEmployee("rohan");
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports : string[]) {
+    super(id, "Accounting"); // child class k constructor se phle parent class k constructor ko call lgane k super ka use
+  }
 
+  addReports(text : string){
+    this.reports.push(text);
+  }
+  printReports(){
+    console.log(this.reports) 
+  }
 
-// accounting.employee = ["mac"];// yeh directly accessible hai so isko aesa ni chahte hm and isko ab hm pvt kr denge
+  addEmployee(emp: string): void {
+      if(emp == "Patel"){
+        return;
+      }
+      this.employee.push(emp);
+  }
+}
+const accDep = new AccountingDepartment("c1",["bugs"]);
+accDep.describe();
+accDep.addReports("complier issue");
+accDep.printReports();
+accDep.addEmployee("varun");
+accDep.printEmployee();
 
-
-accounting.printEmployee();
-
-
-//readnly
