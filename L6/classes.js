b9fa9b12932b5b260bbcd57be44a40dbf1035acc
Anjany Pandey <anjany.pandey06@gmail.com>
2025-02-13 14:09:44 +0530

    learned about setter getter methods, static methods and abstract classes

--- L6/classes.js
+++ L6/classes.js
@@ -109,6 +109,20 @@ class AccountingDepartment extends Department {
         }
         this.employee.push(emp);
     }
+    // getter method
+    get getReports() {
+        if (this.reports.length > 0) {
+            return this.reports;
+        }
+        throw new Error("Report not found..");
+    }
+    // setter method
+    set setReports(value) {
+        if (!value) {
+            throw new Error("Please pass valid value.");
+        }
+        this.reports.push(value);
+    }
 }
 const accDep = new AccountingDepartment("c1", ["bugs"]);
 accDep.describe();
@@ -116,3 +130,57 @@ accDep.addReports("complier issue");
 accDep.printReports();
 accDep.addEmployee("varun");
 accDep.printEmployee();
+//setter and getter use in ts
+// As there invoke is diff from normal methods
+accDep.setReports = "new report";
+console.log("Print through getter method -> ", accDep.getReports);
+//!Static method 
+class Department2 {
+    constructor(id, n) {
+        this.name = n;
+        this.id = id;
+        this.employee = [];
+    }
+    // yha pe this:Department ensures ki isme dep class ka hi ek obj hona chaiye
+    describe() {
+        console.log("Department", this.name);
+    }
+    addEmployee(emp) {
+        this.employee.push(emp);
+    }
+    printEmployee() {
+        console.log("Number of emplpyee : ", this.employee.length);
+        console.log(this.employee);
+    }
+    static getSalary() {
+        return { salary: 500000 };
+    }
+}
+// static methods ko access krne k liye object bnanane ki jarurat ni pdti directly class k name likh k access kr skte hai..
+const salary = Department2.getSalary;
+console.log(salary);
+//! abstract classess
+// yeh woh classess hoti h jiska hm sirf declaration krte hai impplemenatation ni
+// abstract class ka implementation unki sub class me hogaa
+// abstract class se object bhi ni bna skte
+class Department3 {
+    constructor(id, n) {
+        this.employee = [];
+        this.name = n;
+        this.id = id;
+    }
+}
+class subclass extends Department3 {
+    constructor(id, reports) {
+        super(id, 'Sales');
+        this.reports = reports;
+    }
+    describe() {
+        console.log("Department ", this.id);
+    }
+    displayName() {
+        console.log("Department name: ", this.name);
+    }
+}
+const subClass = new subclass("D1", []);
+subClass.describe();
--- L6/classes.ts
+++ L6/classes.ts
@@ -144,6 +144,22 @@ class AccountingDepartment extends Department {
       }
       this.employee.push(emp);
   }
+
+  // getter method
+  get getReports(){
+    if(this.reports.length > 0){
+      return this.reports;
+    }
+    throw new Error("Report not found..")
+  }
+
+  // setter method
+  set setReports(value: string){
+    if(!value){
+      throw new Error("Please pass valid value.")
+    }
+    this.reports.push(value);
+  }
 }
 const accDep = new AccountingDepartment("c1",["bugs"]);
 accDep.describe();
@@ -152,3 +168,74 @@ accDep.printReports();
 accDep.addEmployee("varun");
 accDep.printEmployee();
 
+//setter and getter use in ts
+// As there invoke is diff from normal methods
+accDep.setReports = "new report";
+console.log("Print through getter method -> ", accDep.getReports);
+
+
+//!Static method 
+
+class Department2 {
+  name: string; // we can initialize this name:string ="subham"
+  protected employee: string[];
+  private readonly id: string;
+  constructor(id: string, n: string){
+    this.name = n;
+    this.id =id;
+    this.employee = [];
+  }
+  // yha pe this:Department ensures ki isme dep class ka hi ek obj hona chaiye
+  describe( this:Department2){
+    console.log("Department", this.name);
+  }
+
+  addEmployee(emp: string){
+    this.employee.push(emp);
+  }
+  printEmployee(){
+    console.log("Number of emplpyee : ", this.employee.length);
+    console.log(this.employee);
+  }
+
+  static getSalary(){
+    return {salary:500000}
+  }
+}
+// static methods ko access krne k liye object bnanane ki jarurat ni pdti directly class k name likh k access kr skte hai..
+const salary = Department2.getSalary;
+console.log(salary);
+
+
+//! abstract classess
+// yeh woh classess hoti h jiska hm sirf declaration krte hai impplemenatation ni
+// abstract class ka implementation unki sub class me hogaa
+// abstract class se object bhi ni bna skte
+
+abstract class Department3{
+  name: string;
+  protected employee: string[] = [];
+  protected readonly id : string;
+
+  constructor(id:string, n:string){
+    this.name = n;
+    this.id = id;
+  }
+  abstract describe(this: Department3): void;
+  abstract displayName(): void;
+
+  // yeh joh abstract methods hote hai inki implementation sub classes me hona jaruri hai
+}
+class subclass extends Department3{
+  constructor(id: string, private reports: string[]){
+    super(id, 'Sales');
+  }
+  describe(): void {
+      console.log("Department ", this.id);
+  }
+  displayName(): void {
+      console.log("Department name: ", this.name);
+  }
+}
+const subClass = new subclass("D1", []);
+subClass.describe();
